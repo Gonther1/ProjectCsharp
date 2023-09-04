a591d1bd1cf8f1752f22f4fcfa5ba1e0dabe7b92
Gonther1 <126286883+Gonther1@users.noreply.github.com>
2023-09-04 12:53:32 -0500

    feat: :sparkles: Add new folder in to ProjectsCsharp
    @Gonther1

---
+++ FirstExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!"); // print en C#
+        // Console.ReadLine(); // input en C#
+        // int edad; // Declaracion de variable
+        // float precio = 23; // Declaracion de flotantes
+        // int a = 5;
+        // int b = 10;
+        // int c = 2;
+        // Console.WriteLine(a*b*c); <- Imprime 100
+        /*
+        string nombre="Andres";
+        Console.WriteLine("Ingrese su edad: ");
+        try {
+            edad=int.Parse(Console.ReadLine() ?? "0");
+            precio=int.Parse(Console.ReadLine() ?? "0");
+            Console.WriteLine("El camper {0} tiene {1} años",nombre,edad);
+        } catch (Exception ex) {
+            Console.WriteLine("Errorrr {0}",ex.Message.ToString());
+        } 
+        */
+        var a = 12.3;
+        double b = 12.3;
+        Console.WriteLine($"El valor {a} es de a");
+        Console.Clear();
+        Console.WriteLine($"El valor {b} es de b");
+    }
+}
