ac459fadd05ef3bf399e3eb86b86dcebd3165b6d
Gonther1 <126286883+Gonther1@users.noreply.github.com>
2023-09-06 11:55:59 -0500

    feat: :sparkles: Add FourthExercise
    @Gonther1

---
+++ FourthExercise/Program.cs
@@ -0,0 +1,59 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* 
+        List <string> subjects = new List<string>();
+        int idx = 0;
+        subjects.Add("English");
+        subjects.Add("Math");
+        subjects.Add("Filosofy");
+        subjects.Add("Spanish");
+        ContarElementos(subjects);
+        Console.WriteLine("Ingrese la posicion: ");
+        idx=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("El elemento en la posicion {0} es: {1}",idx,ObtenerElemento(idx,subjects));
+        subjects.Add(AddItemData());
+        Console.WriteLine(string.Join(",", subjects));
+        Console.WriteLine("Ingrese la palabra a buscar: ");
+        if (ValidarItem(subjects,Console.ReadLine().ToLower())) {
+            Console.WriteLine("Encontrado :D");
+        }
+        else 
+        {
+            Console.WriteLine("No encontrado :()");
+        } 
+        */
+        String ? palabra=null;
+        List<String>Dinosaurios=new List<string>();
+        string[] Dinos = {
+            "PrimerDino",
+            "SegundoDino",
+            "TercerDino"
+        };
+        Dinosaurios.AddRange(Dinos);
+        Console.WriteLine("Ingrese el nombre del dinosaurio a buscar: ");
+        palabra=Console.ReadLine();
+        string ? nombre = Dinosaurios.Find(n=> n.StartsWith(palabra ?? String.Empty));
+        Console.WriteLine(nombre != null ? "Se encotro el dino": "No se encontro el dino");
+    }
+    public static void ContarElementos(List<string> lista) {
+        Console.WriteLine("Todos los elementos de la lista :{0}",lista.Count());
+    }
+    public static string AddItemData() {
+        Console.WriteLine("Ingrese el titulo del elemento: ");
+        return Console.ReadLine();
+    }    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="idx">Indice de la materia a mostrar</param>
+    /// <param name="lista">Lista con las respectivas materias</param>
+    /// <returns></returns>
+    public static string ObtenerElemento(int idx,List<string> lista) {
+        return lista[idx];
+    }
+    public static bool ValidarItem(List<string> lista,string valor) {
+        return lista.Exists(l => l.ToLower().Equals(valor));
+    }
+}
--- ThreeExercise/Program.cs
+++ ThreeExercise/Program.cs
@@ -1,40 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Arreglo - Lista fija
-int[] listFixed= { 1, 2, 3, 4};
-Console.WriteLine(string.Join(",",listFixed));
-// Lista, se le puede cambiar el tamaño
-List <string> strings = new List <string> {"1","Hola"};
-Console.WriteLine(string.Join(",",strings));
-strings.Add("xd"); // Metemos un dato para comprobar que si permita meter mas datos
-Console.WriteLine(string.Join(",",strings));
-// Array con datos mixtos con dynamic
-dynamic[] listDynamic = {1,"Hola",true,1.86};
-Console.WriteLine(string.Join(",",listDynamic)); // With this code line we print the array of a way more aestethic for can showing like an another modern languages
-// Dictionary
-Dictionary <int, string> myDictionary = new Dictionary <int, string>();
-myDictionary.Add(17,"Luis Andres Alvarez Silva");
-Console.WriteLine(myDictionary[17]);
-// Dictionary with key string and value arrayString
-Dictionary <string, string[]> valueList = new Dictionary <string, string[]>();
-string[] emails = {"luisandresalvarezsilva@gmail.com"};
-string[] foodFavorite = {"Pizza","HotDog","Chicken"};
-valueList.Add("EmailAndres",emails);
-valueList.Add("FoodFavorite",foodFavorite);
-Console.WriteLine(valueList["EmailAndres"][0]);
-Console.WriteLine(valueList["FoodFavorite"][2]);
-// Constantes en C#
-const float decimal= 3.14956F;
-Console.WriteLine(decimal);
-// Funciones
-/* Tipo de funcion */ int /* Nombre de la funcion */ sumar(/* Parametros */int num1, int num2)
+﻿internal class Program
 {
-    return num1 + num2;
-} 
-/* Se llama la funcion */ Console.WriteLine(sumar());
-// Funciones sin retorno
-// En este caso se le agrega void antes de declarar la funcion
-void multiplicar(int primero, int segundo)
-{
-    Console.WriteLine(primero * segundo);
+    private static void Main(string[] args)
+    {
+        // Arreglo - Lista fija
+        int[] listFixed = { 1, 2, 3, 4 };
+        Console.WriteLine(string.Join(",", listFixed));
+        // Lista, se le puede cambiar el tamaño
+        List<string> strings = new List<string> { "1", "Hola" };
+        Console.WriteLine(string.Join(",", strings));
+        strings.Add("xd"); // Metemos un dato para comprobar que si permita meter mas datos
+        Console.WriteLine(string.Join(",", strings));
+        // Array con datos mixtos con dynamic
+        dynamic[] listDynamic = { 1, "Hola", true, 1.86 };
+        Console.WriteLine(string.Join(",", listDynamic)); // With this code line we print the array of a way more aestethic for can showing like an another modern languages
+                                                          // Dictionary
+        Dictionary<int, string> myDictionary = new Dictionary<int, string>();
+        myDictionary.Add(17, "Luis Andres Alvarez Silva");
+        Console.WriteLine(myDictionary[17]);
+        // Dictionary with key string and value arrayString
+        Dictionary<string, string[]> valueList = new Dictionary<string, string[]>();
+        string[] emails = { "luisandresalvarezsilva@gmail.com" };
+        string[] foodFavorite = { "Pizza", "HotDog", "Chicken" };
+        valueList.Add("EmailAndres", emails);
+        valueList.Add("FoodFavorite", foodFavorite);
+        Console.WriteLine(valueList["EmailAndres"][0]);
+        Console.WriteLine(valueList["FoodFavorite"][2]);
+        // Constantes en C#
+        const float numDecimal = 3.14956F;
+        Console.WriteLine(numDecimal);
+        // Funciones
+        /* Tipo de funcion */
+        int /* Nombre de la funcion */ sumar(/* Parametros */int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        int a = sumar(1, 2);
+        Console.WriteLine(a);
+        /* Se llama la funcion */
+        // Funciones sin retorno
+        // En este caso se le agrega void antes de declarar la funcion
+        int multiplicar(int primero, int segundo)
+        {
+            return primero * segundo;
+        }
+        int b = multiplicar(1, 2);
+        Console.WriteLine(b);
+    }
 }
-Console.WriteLine(multiplicar());
