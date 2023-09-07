7f8f8c19b2f4bb1326b9c340110a631aca132127
Gonther1 <126286883+Gonther1@users.noreply.github.com>
2023-09-07 11:39:31 -0500

    feat: :recycle: Changes in FifthExercise
    @Gonther1

--- FifthExercise/Program.cs
+++ FifthExercise/Program.cs
@@ -5,27 +5,26 @@
      private static void Main(string[] args)
      {
          Dictionary<int, List<string>> myDict = new Dictionary<int, List<string>>();
-         List<string> myList = new List<string>();
+         List<Estudiante> myList = new List<Estudiante>();
          int AddEstudiante = 0;
-         int Flag=0;
          do {
-             Estudiante estudiante= new Estudiante();
-             estudiante.Id=Guid.NewGuid().ToString();
-             Console.WriteLine("Ingrese su nombre: ");
-             estudiante.Nombre=Console.ReadLine();
-             myList.Add(estudiante.Id);
-             myList.Add(estudiante.Nombre);
-             myDict.Add(Flag,)
-              Console.WriteLine("Ingrese su email: ");
-              estudiante.Email=Console.ReadLine();
-              Console.WriteLine("Ingrese su edad: ");
-              estudiante.Edad=int.Parse(Console.ReadLine());
-             Console.Clear();
-             Console.WriteLine("Desea agregar otro estudiante?: ");
-             AddEstudiante=int.Parse(Console.ReadLine());
-              Console.WriteLine(Guid.NewGuid().ToString());
-              Console.WriteLine("{0,10} {1,40}","Cod. Estudiante","Nombre Estudiante");
-              Console.WriteLine("{0,-20} {1,13}",list[First+=2],list[Second+=2]);
+            Estudiante estudiante= new Estudiante();
+            estudiante.Id=Guid.NewGuid().ToString();
+            Console.WriteLine("Ingrese su nombre: ");
+            estudiante.Nombre=Console.ReadLine();
+            // Console.WriteLine("Ingrese su email: ");
+            // estudiante.Email=Console.ReadLine();
+            // Console.WriteLine("Ingrese su edad: ");
+            // estudiante.Edad=int.Parse(Console.ReadLine());
+            myList.Add(estudiante);
+            Console.Clear();
+            Console.WriteLine("Desea agregar otro estudiante?: ");
+            AddEstudiante=int.Parse(Console.ReadLine());
+            // Console.WriteLine(Guid.NewGuid().ToString());
+            // Console.WriteLine("{0,10} {1,40}","Cod. Estudiante","Nombre Estudiante");
+            // Console.WriteLine("{0,-20} {1,13}",list[First+=2],list[Second+=2]); 
+            Console.Clear();
          } while (AddEstudiante==0);
+         Console.WriteLine(myList);
      }
  }
